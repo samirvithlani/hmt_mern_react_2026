50a65b379df3f7471e8aabbdb0d9f852408240fc
samirvithlani <samir.vithlani83955@gmail.com>
2026-05-19 11:21:12 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,12 @@ import { Content } from './components/Content'
 import { MapDemo1 } from './components/MapDemo1'
 import { MapDemo2 } from './components/MapDemo2'
 import { MapDemo3 } from './components/MapDemo3'
-import { Route, Routes } from 'react-router-dom'
+
 import { Movies } from './components/hotstar/Movies'
 import { Shows } from './components/hotstar/Shows'
+import { Route, Routes } from 'react-router-dom'
+import { Sports } from './components/hotstar/Sports'
+import { Navbar } from './components/Navbar'
 
 
 function App() {
@@ -18,9 +21,11 @@ function App() {
 
   return (
     <div>
+      <Navbar></Navbar>
           <Routes>
             <Route path='/movies' element={<Movies/>}></Route>  
             <Route path='/shows' element ={<Shows/>}></Route>
+            <Route path='/sports' element = {<Sports/>}></Route>
           </Routes> 
     </div>
   )
---
+++ src/components/hotstar/Sports.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Sports = () => {
+  return (
+    <div>
+        <h1>SPORTS</h1>
+    </div>
+  )
+}
