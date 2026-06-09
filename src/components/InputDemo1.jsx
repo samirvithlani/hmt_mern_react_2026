c8b1eadb431fb4aea2ac5e5c8a314f07378fd2d2
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-09 12:35:19 +0530

    click handler

--- src/components/InputDemo1.jsx
+++ src/components/InputDemo1.jsx
@@ -3,11 +3,19 @@ import React, { useState } from 'react'
 export const InputDemo1 = () => {
 
     const [name,setName] = useState("")
+    const [age, setage] = useState("")
+    const [isCliked, setisCliked] = useState(false)
 
     const nameHandler = (event)=>{
      //   console.log(event)
         //console.log(event.target.value)
         setName(event.target.value)
+    }
+    const ageHandler = (event)=>{
+        setage(event.target.value)
+    }
+    const clickHandler = ()=>{
+        setisCliked(true)
     }
 
   return (
@@ -16,8 +24,24 @@ export const InputDemo1 = () => {
         <div>
             <label>NAME</label>
             <input type='text' onChange={(event)=>{nameHandler(event)}}></input>
-            {name}
+            
         </div>
+        <div>
+            <label>AGE</label>
+            <input type='text' onChange={(event)=>{ageHandler(event)}}></input>
+            
+        </div>
+        <div>
+            <button onClick={clickHandler}>display</button>
+        </div>
+        {
+            isCliked == true && <div>
+            <h1>OUTOUT</h1>
+            <h1>Name  = {name}</h1>
+            <h3>Age = {age}</h3>
+        </div>
+        }
+        
     </div>
   )
 }
