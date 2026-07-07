2b1e9391df58dd1af87d30810b7b9dfd7b64e350
samirvithlani <samir.vithlani83955@gmail.com>
2026-07-07 11:46:31 +0530

    color change

--- src/components/FindBomb.jsx
+++ src/components/FindBomb.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const FindBomb = () => {
     var gridsize = 5
     var totaltiles = gridsize * gridsize
+    const [clickedTiles, setclickedTiles] = useState([])
+    console.log("clicked tiles",clickedTiles)
+
+    const clickHandler = (index)=>{
+        //alert(index)
+        //spread operators
+        setclickedTiles([...clickedTiles,index])
+
+    }
+    
+
   return (
     <div style={{textAlign:"center"}}>
         <h1>FIND BOMB</h1>
@@ -15,14 +26,17 @@ export const FindBomb = () => {
         }}>
             {
                 Array.from({length:totaltiles}).map((elm,index)=>{
-                    return<div style={{
+                    return<div 
+                    onClick={()=>{clickHandler(index)}}
+                    style={{
                         width:"60px",
                         height:"60px",
                         display:"flex",
                         alignItems:"center",
                         justifyContent:"center",
                         border:"2px solid #ccc",
-                        backgroundColor:"white",
+                        //backgroundColor:"white",
+                        backgroundColor:clickedTiles.includes(index) ?"gray":"white",
                         cursor:"pointer",
                         fontSize:"18px",
                         borderRadius:"4px",
