0f58dcb73cb646effd9f95372e97a02d7460fcd7
samirvithlani <samir.vithlani83955@gmail.com>
2026-08-11 11:26:21 +0530

    props

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CourseList } from './CourseList'
 
 export const Course = () => {
 
+    const[title,setTitle] =useState("INDIA")
+
     var instituteName = "IIT"
     var courses = ["java","php","js"]
+    var book = {
+      name:"java",
+      duration:45,
+      price:1200
+    }
+
+    const test = ()=>{
+      alert("test called...")
+    }
+
 
   return (
     <div style={{textAlign:"center"}}>
         <h1>COURSE</h1>
-        <CourseList instituteName={instituteName} courses = {courses}></CourseList>
+        <CourseList title = {title} instituteName={instituteName} courses = {courses} book={book} test={test}></CourseList>
     </div>
   )
 }
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 //props property
 export const CourseList = (props) => {
-    console.log("props courselist",props) //{instituname:"",courses:[]}
+    console.log("props courselist",props) //{instituname:"",courses:[].book:{},test:()=>{}}
   return (
     <div>
         <h3>Course List</h3>
@@ -12,6 +12,11 @@ export const CourseList = (props) => {
                 return <li>{c}</li>
             })
         }
+        <h1>BookName = {props.book.name}</h1>
+        <h2>Duration = {props.book.duration} -days </h2>
+        <h3>Price = {props.book.price}</h3>
+        <h3>Tilte = {props.title}</h3>
+        <button onClick={props.test}>TEST</button>
     </div>
   )
 }
